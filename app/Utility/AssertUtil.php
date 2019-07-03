9bb53aa458c5d23bee1c27a53dfb4a569a155cfc
Giovanni Dejan <wb-giovannid529582@antfin.com>
2019-07-03 21:50:16 +0700

    Add response & assertion utility

---
+++ app/Utility/AssertUtil.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Utility;
+
+class AssertUtil
+{
+    public static function isTrue(bool $expression, string $errorMessage): void {
+        if($expression != true) {
+            throw new \Exception($errorMessage);
+        }
+    }
+
+    public static function notNull($variable, string $errorMessage) {
+        static::isTrue(isset($variable) && $variable != null, $errorMessage);
+    }
+
+    public static function areArraysEqual(array $a1, array $a2, string $errorMessage): void {
+        static::notNull($a1, '$a1 is null variable');
+        static::notNull($a2, '$a2 is null variable');
+        static::assertTrue($a1 === $a2, $errorMessage);
+    }
+}
