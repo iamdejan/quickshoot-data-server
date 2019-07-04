7d922442e77770bd0e023873c20e0e8027a3e6d0
Giovanni Dejan <wb-giovannid529582@antfin.com>
2019-07-04 15:49:59 +0700

    Adding DBUnitTest

---
+++ tests/Unit/DBUnitTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class DBUnitTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertEquals("quickshoot-party-data-server", config("database.connections.mysql.database"));
+    }
+}
